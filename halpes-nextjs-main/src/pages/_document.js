be7f05b876e791be665676430bf3b14e7011c18b
WilliamYn <wy.younanian@gmail.com>
2025-04-12 17:17:59 -0400

    Changed favicon and page title

--- halpes-nextjs-main/src/pages/_document.js
+++ halpes-nextjs-main/src/pages/_document.js
@@ -1,5 +1,16 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+/**
+ * _document.js is a Next.js custom document file that allows you to customize the initial HTML document
+ * that gets rendered on the server. This file is only rendered on the server and is used to modify
+ * the <html> and <body> tags, as well as add meta tags, favicons, and other static assets.
+ * 
+ * This implementation:
+ * - Sets the document language to English
+ * - Adds various favicon formats for different devices
+ * - Includes a manifest file for PWA support
+ * - Maintains the default Next.js document structure with Main and NextScript components
+ */
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const originalRenderPage = ctx.renderPage;
@@ -27,21 +38,21 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="apple-touch-icon.png"
+            href="/images/resources/favicon/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="favicon-32x32.png"
+            href="/images/resources/favicon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="favicon-16x16.png"
+            href="/images/resources/favicon/favicon-16x16.png"
           />
-          <link rel="icon" href="favicon.ico" />
+          <link rel="icon" href="/images/resources/favicon/favicon.ico" />
           <link rel="manifest" href="manifest.json" />
         </Head>
         <body>
