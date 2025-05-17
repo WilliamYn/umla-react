1f1d3f29b34b4a8cafe791b4a23725f8f90175ac
WilliamYn <wy.younanian@gmail.com>
2025-05-17 11:20:04 -0400

    renamed causes->evenements
    kept old evenements as evenements-temp for future usage

--- halpes-nextjs-main/src/pages/evenements.js
+++ halpes-nextjs-main/src/pages/evenements.js
@@ -1,15 +1,15 @@
-import EventsPage from "@/components/EventsPage/EventsPage";
+import CausesPage from "@/components/CausesPage/CausesPage";
 import Layout from "@/components/Layout/Layout";
 import PageHeader from "@/components/PageHeader/PageHeader";
 import React from "react";
 
-const Events = () => {
+const Causes = () => {
   return (
-    <Layout pageTitle="Events">
-      <PageHeader pageTitle="Events" />
-      <EventsPage />
+    <Layout pageTitle="Causes">
+      <PageHeader pageTitle="Événements" />
+      <CausesPage />
     </Layout>
   );
 };
 
-export default Events;
+export default Causes;
