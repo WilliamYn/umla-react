ed66f02b90bf67103b665ef14e56564d742a1a28
WilliamYn <wy.younanian@gmail.com>
2025-05-17 14:29:24 -0400

    updated 2 pics on main page

--- halpes-nextjs-main/src/components/Charity/CharityImage.js
+++ halpes-nextjs-main/src/components/Charity/CharityImage.js
@@ -6,9 +6,9 @@ const CharityImage = () => {
     <Col xl={6} lg={6}>
       <div className="welcome-one__left">
         <div className="welcome-one__img-box">
-          <Image src="/images/resources/umla-food-2.jpg" alt="" />
+          <Image src="/images/main-page-images/main-vertical-image.jpg" alt="" />
           <div className="welcome-one__img-box-2">
-            <Image src="/images/resources/umla-food-1.jpg" alt="" />
+            <Image src="/images/main-page-images/main-horizontal-image.jpg" alt="" />
           </div>
         </div>
       </div>
