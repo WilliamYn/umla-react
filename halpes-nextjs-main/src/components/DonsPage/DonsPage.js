cbb254fbc6ae7e7bd203569d0477c6ee88f2eff7
William Younanian <william.younanian@unity3d.com>
2025-06-05 23:56:14 -0400

    Revert "button donner en haut de la page"
    This reverts commit 0d2f4efafe420d21b2281ab38c6879d41664c4ec.

--- halpes-nextjs-main/src/components/DonsPage/DonsPage.js
+++ halpes-nextjs-main/src/components/DonsPage/DonsPage.js
@@ -9,10 +9,7 @@ const DonsPage = () => {
         <Container>
           <Row>
             <Col xl={12} lg={12}>
-                        <div className="causes-details__text-box">
-                            <div className="buttons_don_section">
-                                <a href="https://www.zeffy.com/fr-CA/donation-form/08a0cabd-d6a7-4624-8f7f-be381829084b" className="causes-details__share-btn thm-btn donate-btn-donate-page" rel="noreferrer" target="_blank">Donner</a>
-                            </div>
+              <div className="causes-details__text-box">
                 <h3>Où vont mes dons ?</h3>
                 <p>À UMLA, 100 % de nos membres sont bénévoles : aucun salaire, aucun avantage personnel. Chaque dollar reçu est utilisé sur le terrain, sans intermédiaire, pour venir en aide aux plus démunis. Nous refusons les frais administratifs inutiles ou les dépenses cachées. Votre argent ne finance pas des bureaux, mais des repas chauds, des vêtements, du transport et des actions concrètes.</p>
                 <p className="highlight-phrase">Voici comment vos dons sont utilisés :</p>
@@ -62,7 +59,9 @@ const DonsPage = () => {
                     <p>Faire un don à UMLA, c&apos;est bien plus qu&apos;un simple geste de générosité : c&apos;est un acte concret de solidarité... et un avantage fiscal. En tant qu&apos;organisme enregistré, UMLA remet des reçus officiels pour que vos dons soient déductibles d&apos;impôt. Autrement dit, une partie de votre don vous revient lors de votre déclaration annuelle - vous contribuez à un impact réel tout en bénéficiant d&apos;un retour fiscal avantageux.</p>
                     <p>C&apos;est une façon intelligente de contribuer à un changement positif, tout en allégeant votre fardeau fiscal. Que vous donniez 20$, 100$ ou plus, chaque contribution peut être déclarée et soutient une cause locale, humaine et transparente. Donner fait du bien, et fait aussi du sens.</p>
                             </div>
-
+                            <div className="buttons_don_section">
+                                <a href="https://www.zeffy.com/fr-CA/donation-form/08a0cabd-d6a7-4624-8f7f-be381829084b" className="causes-details__share-btn thm-btn donate-btn-donate-page" rel="noreferrer" target="_blank">Donner</a>
+                            </div>
               </div>
             </Col>
           </Row>
