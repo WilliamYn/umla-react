90ac02716270377adb22fd92885cf8ab1b4ff2dc
William Younanian <william.younanian@unity3d.com>
2025-06-05 23:47:46 -0400

    revert hyperlink for vercel

--- halpes-nextjs-main/src/components/Charity/CharityContent.js
+++ halpes-nextjs-main/src/components/Charity/CharityContent.js
@@ -39,12 +39,9 @@ const CharityContent = () => {
         </p>
         <div className="welcome-one__our-mission-and-story">
           <div className="welcome-one__mission-and-story-single">
-                      <h3>
-                          <a href="/notre-mission">
-                              <i className="fas fa-arrow-circle-right"></i> Notre Mission
-                          </a>
-                      </h3>
-
+            <h3>
+              <i className="fas fa-arrow-circle-right"></i>Notre Mission
+            </h3>
             <p className="welcome-one__our-mission-and-story-text">
               Nourrir les corps et réchauffer les coeurs. Donner de l&apos;espoir aux personnes en difficulté. Vêtir les personnes en difficulté.
             </p>
